ea35f8f4b19932b3ad6f3f2baa82196e017d26d2
Charlie <ccsystemtech9@gmail.com>
2020-09-08 20:07:01 -0500

    commit sa940s3

---
+++ docs/precache-manifest.c1c2e1873f8b3a444e82b5bac1c54d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5df0233da97f865518a5802e390046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f3ae87ec87e3ba625d",
+    "url": "/static/css/main.feb7b128.chunk.css"
+  },
+  {
+    "revision": "76528a917622dd32be2e",
+    "url": "/static/js/2.4faffe2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4faffe2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f3ae87ec87e3ba625d",
+    "url": "/static/js/main.9386a243.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
